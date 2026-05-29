7e773f0c8abe6e151c1e17154818bdcb4417b3be
Yejin Lee <wthb22923@gmail.com>
2026-05-29 15:45:08 +0900

    자동화배포

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 Second CI/CD TEST - 수동배포";
+        return "GitHub Actions를 이용한 Third CI/CD TEST - 자동화배포";
     }
 }
